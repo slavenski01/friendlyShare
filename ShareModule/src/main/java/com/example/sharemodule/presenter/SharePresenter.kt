66680b9495efe3ace7f0450bf7c3d8f6fde03bce
Vyacheslav Kuznetsov <vkuznetsov@uma.tech>
2021-12-07 23:15:45 +0300

    FS2 - refactoring

--- ShareModule/src/main/java/com/example/sharemodule/presenter/SharePresenter.kt
+++ ShareModule/src/main/java/com/example/sharemodule/presenter/SharePresenter.kt
@@ -1,7 +1,23 @@
 package com.example.sharemodule.presenter
 
 import com.example.sharemodule.data.interactor.ShareInteractor
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class SharePresenter(private val shareInteractor: ShareInteractor) {
+class SharePresenter(private val shareInteractor: ShareInteractor): CoroutineScope {
 
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO + SupervisorJob()
+
+    fun getUserProducts() {
+        launch {
+            val productsUsers = launch {
+                val products = shareInteractor.getProducts()
+                val users = shareInteractor.getPartyMembers()
+            }
+        }
+    }
 }
